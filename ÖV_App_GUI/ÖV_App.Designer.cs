375863fcf0378aed43cf11a32ee97ceab62bc99e
gerrykushivsroy <gerry.sroy12310.com@gmail.com>
2018-11-28 16:24:49 +0100

    Add New Change

---
+++ ÖV_App_GUI/ÖV_App.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ÖV_App_GUI
+{
+    partial class ÖV_App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSearchConnection = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cboStartStation = new System.Windows.Forms.ComboBox();
+            this.cboEndStation = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "&Von:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(437, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "&Nach:";
+            // 
+            // btnSearchConnection
+            // 
+            this.btnSearchConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchConnection.Location = new System.Drawing.Point(589, 92);
+            this.btnSearchConnection.Name = "btnSearchConnection";
+            this.btnSearchConnection.Size = new System.Drawing.Size(138, 55);
+            this.btnSearchConnection.TabIndex = 5;
+            this.btnSearchConnection.Text = "Verbindung suchen";
+            this.btnSearchConnection.UseVisualStyleBackColor = true;
+            this.btnSearchConnection.Click += new System.EventHandler(this.btnSearchConnection_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(37, 181);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(690, 407);
+            this.listBox1.TabIndex = 6;
+            // 
+            // cboStartStation
+            // 
+            this.cboStartStation.FormattingEnabled = true;
+            this.cboStartStation.Location = new System.Drawing.Point(37, 53);
+            this.cboStartStation.Name = "cboStartStation";
+            this.cboStartStation.Size = new System.Drawing.Size(286, 21);
+            this.cboStartStation.TabIndex = 2;
+            this.cboStartStation.Click += new System.EventHandler(this.cboStartStation_Click);
+            // 
+            // cboEndStation
+            // 
+            this.cboEndStation.FormattingEnabled = true;
+            this.cboEndStation.Location = new System.Drawing.Point(441, 53);
+            this.cboEndStation.Name = "cboEndStation";
+            this.cboEndStation.Size = new System.Drawing.Size(286, 21);
+            this.cboEndStation.TabIndex = 4;
+            this.cboEndStation.Click += new System.EventHandler(this.cboEndDestination_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(351, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Fahrtdauer:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(586, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Gleisnummer:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(34, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Abfahrtszeit:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(344, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ÖV_App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 600);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cboEndStation);
+            this.Controls.Add(this.cboStartStation);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSearchConnection);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ÖV_App";
+            this.Text = "ÖV App V2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSearchConnection;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox cboStartStation;
+        private System.Windows.Forms.ComboBox cboEndStation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ÖV_App_GUI/ÖV_App.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwissTransport;
+using System.Windows.Forms;
+
+namespace ÖV_App_GUI
+{
+    public partial class ÖV_App : Form
+    {
+        Transport transport = new Transport();
+      
+        public ÖV_App()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearchConnection_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Stations getStation(string input)
+        {
+            Stations stationList = transport.GetStations(input);
+            return stationList;
+        }
+
+        public Connections getConnection(string fromStation, string toStattion)
+        {
+            Connections connectionList = transport.GetConnections(fromStation, toStattion);
+            return connectionList;
+        }
+
+        public StationBoardRoot getStationBoard(string station, string id)
+        {
+            StationBoardRoot stationboard = transport.GetStationBoard(station, id);
+            return stationboard;
+        }
+
+        private void cboStartStation_Click(object sender, EventArgs e)
+        {
+            cboStartStation.DroppedDown = true;
+            cboStartStation.DataSource = getStation(cboStartStation.Text).StationList;
+            cboStartStation.DisplayMember = "name";
+        }
+
+        private void cboEndDestination_Click(object sender, EventArgs e)
+        {
+            cboEndStation.DroppedDown = true;
+            cboEndStation.DataSource = getStation(cboEndStation.Text).StationList;
+            cboEndStation.DisplayMember = "name";
+        }
+    }
+}
